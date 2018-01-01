0ff87da9080a6983ddeff33e7b6ce081a8c02783
alice monday <alice0meta@gmail.com>
2018-01-01 15:59:31 -0800

    seems to have been work in progress

--- thisisoursand/build.sh
+++ thisisoursand/build.sh
@@ -2,4 +2,4 @@
 mkdir -p .bin
 ζ ' φ`.bin/it.js`.text = ζ_compile(φ`it.ζ`.text); φ`.bin/hilbert_curve.js`.text = ζ_compile(φ`hilbert_curve.ζ`.text) ;' &&
 browserify .bin/it.js -o bundle.js &&
-ζ ' go_to(`http://localhost:8080/`) ;'
+ζ ' go_to(`http://localhost:8080/it.html`) ;'
