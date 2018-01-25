36c24ae734f6862377165f928f211f88b99d4ca4
alice monday <alice0meta@gmail.com>
2018-01-25 08:19:09 -0800

    mv sand -> scratch/

--- thisisoursand/build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p .bin
-ζ ' φ`.bin/it.js`.text = ζ_compile(φ`it.ζ`.text); φ`.bin/hilbert_curve.js`.text = ζ_compile(φ`hilbert_curve.ζ`.text) ;' &&
-browserify .bin/it.js -o bundle.js &&
-ζ ' go_to(`http://localhost:8080/`) ;'
