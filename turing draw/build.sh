227cdd74b2c39f9f6de178748073c94c0877322f
alice monday <alice0meta@gmail.com>
2018-01-25 08:13:10 -0800

    -

--- turing draw/build.sh
+++ turing draw/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-mkdir -p .bin
-ζ ' φ`.bin/it.js`.text = ζ_compile(φ`it.ζ`.text) ;' &&
-browserify .bin/it.js -o bundle.js &&
+mkdir -p .bin_𐅦𐅭𐅯𐅮𐅬
+ζ ' φ`.bin_𐅦𐅭𐅯𐅮𐅬/it.js`.text = ζ_compile(φ`it.ζ`.text) ;' &&
+browserify .bin_𐅦𐅭𐅯𐅮𐅬/it.js -o bundle.js &&
+rm -r .bin_𐅦𐅭𐅯𐅮𐅬 &&
 ζ ' go_to(`file:///~/code/alice0meta.github.io/turing%20draw/index.html`,{in_app:`chrome`}) ;'
