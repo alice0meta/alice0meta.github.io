fbd8492798403f16312715045828a901e529784c
alice monday <alice0meta@gmail.com>
2018-03-08 10:05:52 -0800

    -

--- turing draw/run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p .bin_𐅦𐅭𐅯𐅮𐅬
-ζ ' φ`.bin_𐅦𐅭𐅯𐅮𐅬/it.js`.text = ζ_compile(φ`it.ζ`.text) ;' &&
-browserify .bin_𐅦𐅭𐅯𐅮𐅬/it.js -o bundle.js &&
-rm -r .bin_𐅦𐅭𐅯𐅮𐅬 &&
-ζ ' go_to(`file:///~/code/alice0meta.github.io/turing%20draw/index.html`,{in_app:`chrome`}) ;'
