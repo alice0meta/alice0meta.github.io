3660c415069b6157f51ed9653d432020aa82ae13
alice monday <alice.s.monday@gmail.com>
2019-06-04 16:50:09 -0700

    Update music.js

--- music.js
+++ music.js
@@ -4,14 +4,13 @@
 // is_A= Array.isArray
 // is_S= _=> is_c(_)&& is_f(_[Symbol.iterator])
 // A= x=> is_A(x)? x : is_S(x)? [...x] : [x]
+ERR= _=>{ throw _ instanceof Error? _ : Error(_) }
 next_= _=>_ .next().value
 last= x=>{ var t ;for(t of x) ;return t }
 fold= function*(f,x,a){ x=x[Symbol.iterator]() ;arguments.length===2 &&( a= next_(x) ) ;for(var t of x) yield a= f(a,t) }
 foldx= (..._)=> last(fold(..._))
 max= (x,f= (a,b)=>a>b )=> foldx((a,x)=> f(x,a)? x : a ,x)
 
-ERR= _=>{ throw _ instanceof Error? _ : Error(_) }
-// AS= (..._)=> Object.assign (..._)
 zip= function(x){ var r=[] ;var I=max(x.map(_=>_.length)) ;for(var i=0;i<I;i++) r.push( x.map(_=>_[i]) ) ;return r }
 
 AC= new AudioContext()
@@ -68,21 +67,19 @@ __delay= (l_s)=> AC.createDelay(l_s)
 
 __sine= (s)=>( s= AC.createOscillator() ,s.type= 'sine' ,s )
 __gain= ()=> AC.createGain()
-__sine_up= (s,u)=>( s=__sine() ,u= __gain() ,s.connect(u) ,{ rate:s.frequency ,gain:u.gain   ,head:s ,out:u
-	// ,set(x,rt){ rt!==undefined&&( this.rate.value= rt ) ;this.gain.value= x } ,a5553:true
-	,set(x,rt){ rt!==undefined&&( this.rate.value= rt ) ;this.a5553?( delete this.a5553 ,this.gain.value= x ): this.gain.setTargetAtTime(x,0,.002) } ,a5553:true
-	// BUG set gets trampled over and canceled sometimes maybe :c
-	,do(){ this.head.start() ;this.out.connect(AC.destination) ;return this }
-	,stop(){ this.set(0) }
-	} )
+__sine_up= (s,u)=>( s=__sine() ,u= __gain() ,s.connect(u) ,{ rate:s.frequency ,gain:u.gain   ,head:s ,out:u } )
 
-equalize= [[0,869],[41,869],[55,791],[66,729],[77,684],[98,623],[148,541],[216,584],[249,600],[261,605],[286,606],[305,608],[336,592],[354,581],[372,557],[380,530],[392,494],[403,475],[410,456],[423,436],[429,420],[440,405],[445,382],[459,366],[471,350],[487,345],[508,325],[532,314],[548,306],[571,302],[592,290],[611,271],[622,263],[642,236],[660,193],[673,138],[686,119],[700,103],[710,86],[721,84],[730,73],[741,65],[763,69],[776,69],[786,63],[804,59],[821,54],[840,58],[950,50],[1000,50]].map(_=>_.map(_=>_/1000))
+equalize= [[0,869],[41,869],[55,791],[66,729],[77,684],[98,623],[148,541],[216,584],[249,600],[261,605],[286,606],[305,608],[336,592],[354,581],[372,557],[380,530],[392,494],[445,382],[508,325],[548,306],[571,302],[592,290],[622,263],[642,236],[660,193],[673,138],[686,119],[700,103],[730,73],[741,65],[763,69],[776,69],[786,63],[804,59],[821,54],[840,58],[950,50],[1000,50]].map(_=>_.map(_=>_/1000))
 softsound_interpolate= numeric.spline(equalize.map(_=>_[0]),equalize.map(_=>_[1]))
 
 speak= ([x,y],s)=>{
+	s=s||{ stop:function(){ this._.gain.setTargetAtTime(0,0,.002) ;setTimeout(()=> this._.gain.value= 0 ,10) } }
+	// s=s||{ stop:function(){ this._.gain.value= 0 } }
+	// s=s||{ os:[] ,stop:function(){ this.os.map(_=> _.head.stop() ) } }
+	x= 1-x
 	var rt= 40* 2**(x*7)
-	y= (1-y/10) * softsound_interpolate.at(x)
-	// y= softsound_interpolate.at(x)	
+	y= (1-y/10) *softsound_interpolate.at(x)
+
 	// var harmonics= 10
 	// var h= Math.max(0, Math.min(harmonics, y))
 	// let harmonicWarpWidth= 1.5
@@ -90,8 +87,9 @@ speak= ([x,y],s)=>{
 	// for (let i= 0; i < harmonics; i++) {
 		// x= x * (i+1)
 		// y= 100 /x *(1 + harmonicWarpMagnitude*(harmonicWarpWidth**(-Math.pow(i-(h * (harmonics-1)),2))))
-	s=s|| __sine_up().do()
-	s.set(y,rt)
+	if( !s._ ){ s._= __sine_up() ;s._.head.start() ;s._.out.connect(AC.destination) }
+	s._.gain.value= y
+	s._.rate.value= rt
 	// }
 	return s }
 
@@ -129,7 +127,7 @@ function MouseHandler(e) {
 	       , e.pageY * devicePixelRatio ]
 	    w= [ e.pageX / screen.width
 	       , e.pageY / screen.height ]
-		dt22mgazu(27/32,40,0.1)(w)
+		dt22mgazu(216/256,40)(w)
 		e.preventDefault()
 	}
 	if (e.type == "mouseup")
@@ -156,21 +154,22 @@ function TouchHandler(e){ e.preventDefault() ;for(var t of for__touchlist(e)){
 	else if( e.type==='touchend' ) a8855[i]&& a8855[i].stop()
 	else a8855[i]= speak(w,a8855[i])
 	// t.force? Math.round(t.force *125 +10)
-	dt22mgazu((t.identifier * 1/7) % 1 ,30,40,0.02 )(w) } }
+	dt22mgazu((t.identifier * 1/6.3) % 1 ,32,24,.02 )(w) } }
 
 _draw= f=>(..._)=>{ var X= canvas.getContext("2d") ;X.save() ;f(X)(..._) ;X.restore() }
-a9998= _draw(X=>(xy,h,l,d,a)=>{
+
+drawTouch= _draw(X=>(xy,h,l,d=40,a=.1)=>{
 	X.translate(...xy)
 	X.beginPath()
-	X.arc(0,0,d,0,2*Math.PI,false)
+	X.arc(0, 0, d, 0, 2*Math.PI, false)
 	X.closePath()
 	X.fillStyle= `hsla(${h*256},100%,${l}%,${a})`
 	X.fill()
 	})
-dt22mgazu= (..._)=>x=> a9998([x[0]*screen.width*devicePixelRatio,x[1]*screen.height*devicePixelRatio],..._)
-var x= numeric.linspace(0,1,2000) ;var equ22= zip([ x,softsound_interpolate.at(x) ])
-setTimeout(()=>{ equalize.map(dt22mgazu(1/5,20,40,0.3)) },0)
-equ22.map(dt22mgazu(7/8,40,17,0.1))
+
+dt22mgazu= (..._)=>x=> drawTouch([x[0]*screen.width*devicePixelRatio,x[1]*screen.height*devicePixelRatio],..._)
+// equalize.map(dt22mgazu(1/5,20,40,.3))
+// var x= numeric.linspace(0,1,2000) ;zip([ x,softsound_interpolate.at(x) ]).map(dt22mgazu(7/8,40,17,.1))
 
 }
 // DELTA devicePixelRatio =poll
